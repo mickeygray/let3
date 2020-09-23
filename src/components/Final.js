6a539f053ef48f45c5f2dc6ed08cd7b58f7d4100
mickeygray <mgray@nattaxexperts.com>
2020-09-23 09:28:11 -0700

    first commit

---
+++ src/components/Final.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useCallback } from "react";
+import Modal3 from "./Modal3";
+import final from "../images/finaldemand.png";
+
+const Final = () => {
+  const [modal, setModal] = useState(false);
+
+  const [urls, setUrls] = useState("");
+
+  useEffect(() => {
+    if (window.location.href.length > 0) {
+      setUrls(window.location.href);
+    }
+  }, []);
+
+  const toggleModal = useCallback(() => {
+    setModal((prevState) => !prevState);
+  }, []);
+
+  return (
+    <div className='container '>
+      <div className='grid-2'>
+        <div>
+          {modal ? (
+            <Modal3 urls={urls} toggleModal={toggleModal} />
+          ) : (
+            <img src={final} alt='Final Demand For Payment' />
+          )}
+        </div>
+        <div>
+          <br />
+
+          <h2 className='lead' style={{ color: "red" }}>
+            Final Demand For Payment Letter
+          </h2>
+          <h3>
+            Pending Legal Action Against American Tax Solutions,
+            GetATaxLawyer.com and Tax Debt Resolution
+          </h3>
+          <h4>
+            <i>
+              If you or a loved one has recieved a letter like this one, you may
+              be entitled to compensation as an ongoing complaint filed against
+              the company American Tax and Disbarred Attorney Terrance "Terry"
+              Selb Aka Chris Baker.{" "}
+            </i>
+          </h4>
+          <br />
+          <button
+            className='btn btn-block btn-dark'
+            onClick={() => setModal((prevState) => !prevState)}>
+            File A Complaint
+          </button>
+          <br />
+          <ul>
+            <li>
+              <h4>
+                Disbarred Attorney Terrance Selb sends these mailers to people
+                in an attempt to solicit services.
+              </h4>
+            </li>
+            <br />
+            <li>
+              <h5>
+                We are actively submitting all complaints against Terrance Selb,
+                Tyler Bennet, Chris Baker, Ben Graupner and Andruw Rapport and
+                the companies American Tax Solutions, Tax Debt Group and
+                GetATaxLawyer.com to the BBB, FTC and State of California.
+              </h5>
+            </li>
+          </ul>
+          <br />
+          <h5>
+            Your complaints are completely anonymous and could lead to financial
+            compensation if you were directly impacted by this scam.
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Final;
---
+++ src/components/Modal3.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect, Fragment } from "react";
+
+const Modal3 = (props) => {
+  const { urls } = props;
+  useEffect(() => {
+    setForm({
+      fullName: "",
+      email: "",
+      phone: "",
+      problem: "",
+      client: "",
+      paid: "",
+      url: urls,
+    });
+  }, [urls]);
+
+  const [form, setForm] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+    problem: "",
+    client: "",
+    paid: "",
+    url: urls,
+    mailers: [],
+  });
+
+  const onChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(form);
+  return (
+    <div
+      className='card bg-light container all-center'
+      style={{ margin: "auto" }}>
+      <form
+        className='form-text'
+        name='form'
+        method='post'
+        action='/thanks/'
+        data-netlify='true'
+        data-netlify-honeypot='bot-field'>
+        <span style={{ float: "right" }}>
+          <button onClick={props.toggleModal}> X</button>
+        </span>
+        <input type='hidden' name='form-name' value='form' />
+
+        <input
+          type='text'
+          name='url'
+          style={{ display: "none" }}
+          value={urls}
+          onChange={onChange}
+        />
+        <input
+          type='text'
+          name='fullName'
+          placeholder='Full Name'
+          onChange={onChange}
+        />
+        <input
+          type='text'
+          name='email'
+          placeholder='e-mail'
+          onChange={onChange}
+        />
+        <input
+          type='text'
+          name='phone'
+          placeholder='Phone Number'
+          onChange={onChange}
+        />
+
+        <label htmlFor='years'> Tell us your story</label>
+        <textarea
+          type='text'
+          style={{ height: "200px" }}
+          name='problem'
+          onChange={onChange}
+        />
+
+        <label htmlFor='company'>
+          Were you an American Tax Solutions or Tax Debt Group Client?
+        </label>
+        <select name='client' id='client' onChange={onChange}>
+          <option value=''></option>
+          <option value='true'>Yes</option>
+          <option value='false'>No</option>
+        </select>
+
+        {form.client === "true" ? (
+          <Fragment>
+            {" "}
+            <label htmlFor='paid'>How Much Money Did You Pay Them?</label>
+            <input
+              type='text'
+              placeholder='Amount Paid'
+              name='paid'
+              onChange={onChange}
+            />
+          </Fragment>
+        ) : (
+          ""
+        )}
+        <br />
+        <button className='btn btn-block btn-dark' type='submit'>
+          File Your Complaint
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Modal3;
